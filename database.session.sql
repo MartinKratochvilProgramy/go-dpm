c930d330ef72e57b96e546834b0c6e11dfcc0c3e
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-10-23 13:11:34 +0200

    rename password column in db

--- database.session.sql
+++ database.session.sql
@@ -2,7 +2,7 @@
 CREATE TABLE IF NOT EXISTS users(
     id serial PRIMARY KEY,
     username VARCHAR (100) UNIQUE NOT NULL,
-    password VARCHAR (100) NOT NULL,
+    password_hash VARCHAR (100) NOT NULL,
     created_at TIMESTAMP DEFAULT NOW() NOT NULL,
     changed_at TIMESTAMP
     currency VARCHAR(10)
