93aaf70ec45451c306083289e05da8e6338a1ba9
Martin Kratochvíl <mkratochvil2@retia.cz>
2023-10-27 13:47:35 +0200

    add exp time

--- utils/token/generateToken.go
+++ utils/token/generateToken.go
@@ -2,24 +2,16 @@ package token
 
 import (
 	"os"
-	"strconv"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 )
 
 func GenerateToken(user_id uint) (string, error) {
-
-	token_lifespan, err := strconv.Atoi(os.Getenv("TOKEN_HOUR_LIFESPAN"))
-
-	if err != nil {
-		return "", err
-	}
-
 	claims := jwt.MapClaims{}
 	claims["authorized"] = true
 	claims["user_id"] = user_id
-	claims["exp"] = time.Now().Add(time.Hour * time.Duration(token_lifespan)).Unix()
+	claims["exp"] = time.Now().Add(time.Hour * 1).Unix()
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	return token.SignedString([]byte(os.Getenv("API_SECRET")))
